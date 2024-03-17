4dba3985304fa5c4b0475edb4eefa290e2a7b862
Adam Kasperek <35041762+Akasp-dev@users.noreply.github.com>
2024-03-17 12:27:48 +0100

    Update App.java

--- src/com/connector/cludodb/App.java
+++ src/com/connector/cludodb/App.java
@@ -13,7 +13,7 @@ public class App {
 			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 			screenWidth = (int)screenSize.getWidth();
 			screenHeight = (int)screenSize.getHeight();
-			DatabaseConnection connection = new DatabaseConnection("jdbc:mysql://s177.cyber-folks.pl:3306/i7328_clients", "i7328", "IncludO2020*");
+			DatabaseConnection connection = new DatabaseConnection("", "", "");
 			Window dataWindow = new Window("CludoDB", screenWidth / 2 + 300, screenHeight / 2 + 200, connection);
 			dataWindow.start();
 		}
